1b20ae8cbc2efcd38634273d182d32217d912ca1
Monty Anderson <monty.linux@gmail.com>
2017-06-30 21:56:15 +0100

    function

--- foop.c
+++ foop.c
@@ -3,27 +3,31 @@
 #include <string.h>
 #include <stdint.h>
 
+void *_this;
+
 void *foop_new(const class_t *class) {
-	void *mem = malloc(class->size + sizeof(void *));
-	void *object = ((char *) mem) + sizeof(void *);
+	void *mem = calloc(1, class->size + sizeof(void *));
+	_this = ((char *) mem) + sizeof(void *);
 
 	memcpy(mem, &class, sizeof(void *));
 
 	if(class->constructor != NULL) {
-		class->constructor(object);
+		class->constructor();
 	}
 
-	return object;
+	return _this;
 }
 
 void foop_delete(void *object) {
 	void *mem = ((char *) object) - sizeof(void *);
+	_this = object;
+
 	class_t *class;
 
 	memcpy(&class, mem, sizeof(void *));
 
 	if(class->destructor != NULL) {
-		class->destructor(object);
+		class->destructor();
 	}
 
 	free(mem);
@@ -37,14 +41,16 @@ bool foop_instanceOf(const class_t *class, void *object) {
 }
 
 void foop_reconstruct(void *object) {
+	_this = object;
+
 	class_t *class;
 	memcpy(&class, ((char *) object) - sizeof(void *), sizeof(void *));
 
 	if(class->destructor) {
-		class->destructor(object);
+		class->destructor();
 	}
 
 	if(class->constructor) {
-		class->constructor(object);
+		class->constructor();
 	}
 }
--- foop.h
+++ foop.h
@@ -5,18 +5,23 @@
 #include <stdbool.h>
 
 typedef struct {
-	void (*constructor)(void *);
-	void (*destructor)(void *);
+	void (*constructor)();
+	void (*destructor)();
 	size_t size;
 } class_t;
+
+extern void *_this;
 
 void *foop_new(const class_t *class);
 void foop_delete(void *object);
 bool foop_instanceOf(const class_t *class, void *object);
 
+#define foop_$(object) ((typeof((object))) (_this = (object)))
+
 #define new foop_new
 #define delete foop_delete
 #define instanceOf foop_instanceOf
 #define reconstruct foop_reconstruct
+#define $ foop_$
 
 #endif
